af1923627c1bd23cd8e6f212d9aaa066e5d748b2
tomdog <vichh1024@163.com>
2021-12-15 11:07:23 +0800

    second

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,23 @@
 <template>
   <div id="app">
+    <router-view/>
+    <main-tabbar/>
   </div>
 </template>
 
 <script>
+import MainTabbar from 'components/content/mainTabbar/MainTabbar'
+
 export default {
   name: 'app',
   components: {
-
+    MainTabbar
   }
 }
 </script>
 
 <style>
+
+  @import "assets/css/base.css";
 
 </style>
---
+++ src/views/home/Home.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="home">
+    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+    <home-swiper :banners="banners"></home-swiper>
+    <recommend-view :recommends ="recommends"></recommend-view>
+  </div>
+</template>
+
+<script>
+import NavBar from 'components/common/navbar/NavBar'
+import HomeSwiper from 'views/home/childComps/HomeSwiper'
+import RecommendView from 'views/home/childComps/RecommendView'
+import {getHomeMultidata} from 'network/home'
+
+
+
+export default {
+  components: {
+    NavBar,
+    HomeSwiper,
+    RecommendView
+
+  },
+  data(){
+    return {
+      // relsult : null
+      banners: [],
+      recommends: []
+    }
+  },
+  created(){
+    getHomeMultidata().then(res=>{
+      console.log(res);
+      this.banners = res.data.banner.list;
+      this.recommends = res.data.recommend.list;
+    })
+  }
+
+}
+</script>
+
+<style>
+  .home-nav {
+    background-color: var(--color-tint);
+    color: white;
+  }
+
+
+</style>
